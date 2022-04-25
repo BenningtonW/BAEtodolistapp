e2a9c1667b9e0693b1c4047635c9535a8e1bcaa5
BenningtonW <brendan.j.woods@gmail.com>
2022-04-25 13:06:43 +0100

    init commit

---
+++ src/main/java/com/qa/baetodolist/BaetodolistApplication.java
@@ -0,0 +1,13 @@
+package com.qa.baetodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaetodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaetodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/baetodolist/BaetodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.baetodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaetodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
