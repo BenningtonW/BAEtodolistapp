43c8b3ccef4be4fffb1f0535158243bebe1a1f59
BenningtonW <brendan.j.woods@gmail.com>
2022-04-26 16:42:54 +0100

    setup repo and contoller

---
+++ src/main/java/com/qa/baetodolist/controller/TaskController.java
@@ -0,0 +1,56 @@
+package com.qa.baetodolist.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.baetodolist.domain.Task;
+import com.qa.baetodolist.service.TaskService;
+
+@RestController
+public class TaskController {
+
+	private TaskController service;
+
+	public TaskController(TaskController service) {
+		this.service = service;
+	}
+
+	// Get by ID (get one User)
+	@GetMapping("/getById/{id}") // localhost:8080/getById/id
+	public ResponseEntity<Task> getById(@PathVariable long id) {
+		return new ResponseEntity<Task>(service.getById(id), HttpStatus.OK);
+	}
+
+	// Get All	
+	@GetMapping("/getAll") // localhost:8080/getAll
+	public ResponseEntity <List<Task>> getAll() {
+		return new ResponseEntity <List<Task>> (service.getAll(), HttpStatus.OK);
+	}
+
+	// Post
+	@PostMapping("/create") // localhost:8080/create
+	public ResponseEntity<Task> create(@RequestBody Task task) {
+		return new ResponseEntity<Task>(service.create(task), HttpStatus.CREATED);
+	}
+
+	// Put/Patch
+	@PutMapping("/update/{id}") // localhost:8080/update/id
+	public ResponseEntity<Task> update(@PathVariable long id, @RequestBody Task task) {
+		return new ResponseEntity<Task>(service.update(id, task), HttpStatus.ACCEPTED);
+	}	
+		// Delete
+	@DeleteMapping("/delete/{id}") //localhost:8080/delete/idesponseEntity<Boolean>
+	public ResponseEntity<?> delete(@PathVariable long id) {
+		return (service.delete(id))? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+}
---
+++ src/main/java/com/qa/baetodolist/repo/TaskRepo.java
@@ -0,0 +1,17 @@
+package com.qa.baetodolist.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.qa.baetodolist.domain.Task;
+
+@Repository
+public interface TaskRepo extends JpaRepository <Task, Long> {
+	
+Optional<Task> findByTask(String task);
+	
+
+	
+}
